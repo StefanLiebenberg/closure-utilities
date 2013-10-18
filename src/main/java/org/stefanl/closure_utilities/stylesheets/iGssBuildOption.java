a996b04c6da0ee799b7c95a1ebdc8d082a66b2b7
Stefan Liebenberg <siga.fredo@gmail.com>
2013-10-18 09:42:32 +0200

    added interface

---
+++ src/main/java/org/stefanl/closure_utilities/stylesheets/iGssBuildOption.java
@@ -0,0 +1,42 @@
+package org.stefanl.closure_utilities.stylesheets;
+
+import com.google.common.collect.ImmutableCollection;
+import com.google.common.collect.ImmutableList;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.io.File;
+import java.net.URI;
+
+public interface iGssBuildOption {
+
+    @Nullable
+    public File getOutputFile();
+
+    @Nullable
+    public ImmutableList<File> getSourceFiles();
+
+    @Nullable
+    public File getRenameMap();
+
+    @Nonnull
+    public Boolean getShouldGenerateForProduction();
+
+    @Nonnull
+    public Boolean getShouldGenerateForDebug();
+
+    @Nonnull
+    public Boolean getShouldCalculateDependencies();
+
+    @Nullable
+    public ImmutableCollection<File> getSourceDirectories();
+
+    @Nullable
+    public ImmutableList<String> getEntryPoints();
+
+    @Nullable
+    public URI getAssetsUri();
+
+    @Nullable
+    public File getAssetsDirectory();
+}
