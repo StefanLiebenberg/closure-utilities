7e4d5721490f7b1c2b7fcc55977edbd2cc1addc7
Stefan Liebenberg <siga.fredo@gmail.com>
2014-07-05 16:05:46 +0200

    adding basic mojos

---
+++ maven/src/main/java/slieb/closureutils/maven/ServeMojo.java
@@ -0,0 +1,13 @@
+package slieb.closureutils.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+
+public class ServeMojo extends AbstractMojo {
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        throw new MojoExecutionException("Not Implemented yet");
+    }
+}
